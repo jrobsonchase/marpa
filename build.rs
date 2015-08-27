019bcfd67534d6bf01797dd7e29fb8c35fef167f
Josh Chase <jcjoshuachase@gmail.com>
2015-08-27 13:27:24 -0700

    removed non-marpa generated bindings

--- build.rs
+++ build.rs
@@ -1,14 +1,18 @@
 use std::process::Command;
+use std::env;
 
 fn main() {
+    let dir = env::current_dir().unwrap();
+
     let libmarpa_version = "8.3.0";
-    let libmarpa_dir = format!("libmarpa-{}", libmarpa_version);
+    let libmarpa_dir = format!("{}/libmarpa-{}", dir.display(), libmarpa_version);
     let libmarpa_tarball = format!("{}.tar.gz", libmarpa_dir);
 
     Command::new("tar").arg("xf").arg(&libmarpa_tarball).status().unwrap();
-    Command::new("./configure").current_dir(&libmarpa_dir).status().unwrap();
-    Command::new("make").current_dir(&libmarpa_dir).status().unwrap();
-
+    Command::new("./configure").current_dir(&libmarpa_dir)
+        .status().unwrap();
+    Command::new("make").current_dir(&libmarpa_dir)
+        .status().unwrap();
     println!("cargo:rustc-link-search=native={}/.libs", libmarpa_dir);
     println!("cargo:rustc-link-lib=static=marpa");
 }
