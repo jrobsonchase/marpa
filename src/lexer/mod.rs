2748339dc0239ba170824c8981da1c8de8949f35
Josh Chase <jcjoshuachase@gmail.com>
2015-09-09 22:03:55 -0700

    remove value from token struct

--- src/lexer/mod.rs
+++ src/lexer/mod.rs
@@ -14,7 +14,6 @@ pub struct Lexer {
 pub struct Token {
     start: usize,
     end: usize,
-    value: i32,
     ty: i32,
 }
 
@@ -47,14 +46,14 @@ impl Lexer {
         }
     }
 
-    fn new_token(&mut self, end: usize, ty: i32) -> Token {
+    fn new_token(&mut self, end: usize, ty: i32) -> (Token, i32) {
         let val = self.tokens.len();
         let start = if val > 0 { self.tokens.last().unwrap().end } else { 0 };
-        self.tokens.push(Token{ start: start, end: end, value: val as i32, ty: ty });
-        self.tokens.last().unwrap().clone()
+        self.tokens.push(Token{ start: start, end: end, ty: ty });
+        (self.tokens.last().unwrap().clone(), val as i32)
     }
 
-    pub fn next_token(&mut self) -> Option<Token> {
+    pub fn next_token(&mut self) -> Option<(Token, i32)> {
         for i in (0..self.tok_re.len()) {
             match self.tok_re.get(i).unwrap().find(self.remaining_text()) {
                 None => {},
@@ -64,7 +63,7 @@ impl Lexer {
         None
     }
 
-    pub fn next_token_hinted<I: IntoIterator<Item=usize>>(&mut self, it: I) -> Option<Token> {
+    pub fn next_token_hinted<I: IntoIterator<Item=usize>>(&mut self, it: I) -> Option<(Token, i32)> {
         for ix in it {
             if ix < self.tok_re.len() {
                 match self.tok_re.get(ix).unwrap().find(self.remaining_text()) {
@@ -86,9 +85,9 @@ pub struct TokIter<'a> {
 }
 
 impl<'a> Iterator for TokIter<'a> {
-    type Item = Token;
+    type Item = (Token, i32);
 
-    fn next(&mut self) -> Option<Token> {
+    fn next(&mut self) -> Option<Self::Item> {
         self.lex.next_token()
     }
 }
