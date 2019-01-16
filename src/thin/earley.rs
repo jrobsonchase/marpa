26f6c64165d0039a28705985a3a7e0272f81f7cf
Josh Robson Chase <josh@robsonchase.com>
2019-01-16 08:40:35 -0500

    Metadata, license, and squash warning

--- src/thin/earley.rs
+++ src/thin/earley.rs
@@ -2,4 +2,6 @@ use crate::thin::libmarpa_sys::*;
 
 pub type Earleme = Marpa_Earleme;
 pub type EarleySet = Marpa_Earley_Set_ID;
+// Note(jrobsonchase): Not actually used yet, but could be at some point.
+#[allow(dead_code)]
 pub type EarleyItem = Marpa_Earley_Item_ID;
