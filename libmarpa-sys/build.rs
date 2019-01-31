0116a26ec15291da089ba4b928e37176f6c09b10
Josh Robson Chase <josh@robsonchase.com>
2019-01-31 07:59:44 -0500

    Redo libmarpa-sys build script (again)
    Hopefully autotools will at last be satisfied.

--- libmarpa-sys/build.rs
+++ libmarpa-sys/build.rs
@@ -1,15 +1,46 @@
-use std::{env, path::PathBuf};
+use std::env;
+use std::io;
+use std::path::Path;
+use std::path::PathBuf;
+use std::process::Command;
 
-use autotools::Config;
+use flate2::read::GzDecoder;
+use std::fs::File;
+use tar::Archive;
 
-const LIBMARPA_VERSION: &'static str = "8.3.0";
+const LIBMARPA_VERSION: &str = "8.3.0";
 
 fn main() {
-    let current_dir = PathBuf::from(env::var("CARGO_MANIFEST_DIR").unwrap());
-    let libmarpa_dir = current_dir.join(format!("libmarpa-{}", LIBMARPA_VERSION));
+    let crate_root: PathBuf = env::var("CARGO_MANIFEST_DIR").unwrap().into();
+    let out_dir: PathBuf = env::var("OUT_DIR").unwrap().into();
 
-    let dst = Config::new(libmarpa_dir).cflag("-fPIC").build().join("lib");
+    let libmarpa_name = format!("libmarpa-{}", LIBMARPA_VERSION);
+    let libmarpa_tarball = crate_root.join(format!("{}.tar.gz", libmarpa_name));
+    let libmarpa_out_dir = out_dir.join(&libmarpa_name);
 
-    println!("cargo:rustc-link-search=native={}", dst.display());
+    extract_tar_gz(&libmarpa_tarball, &out_dir).unwrap();
+
+    let configure_status = Command::new(libmarpa_out_dir.join("configure"))
+        .current_dir(&libmarpa_out_dir)
+        .env("CFLAGS", "-fPIC")
+        .status()
+        .expect("configure");
+    assert!(configure_status.success(), "Configure command failed");
+
+    let make_status = Command::new("make").current_dir(&libmarpa_out_dir).status().expect("make");
+    assert!(make_status.success(), "Make command failed");
+
+    println!("cargo:rustc-link-search=native={}/.libs", libmarpa_out_dir.display());
     println!("cargo:rustc-link-lib=static=marpa");
+}
+
+fn extract_tar_gz<P, Q>(path: P, out_dir: Q) -> io::Result<()>
+where
+    P: AsRef<Path>,
+    Q: AsRef<Path>,
+{
+    let tar_gz = File::open(path)?;
+    let tar = GzDecoder::new(tar_gz);
+    let mut archive = Archive::new(tar);
+    Ok(archive.unpack(out_dir)?)
 }
