abd95b7f4653fe1fc23351932e254cbff8b6e5e4
Deyan Ginev <d.ginev@jacobs-university.de>
2019-01-30 19:20:09 -0500

    can we hope to auto-assign the aclocal version?

--- libmarpa-sys/build.rs
+++ libmarpa-sys/build.rs
@@ -7,8 +7,8 @@ const LIBMARPA_VERSION: &'static str = "8.3.0";
 fn main() {
     let current_dir = PathBuf::from(env::var("CARGO_MANIFEST_DIR").unwrap());
     let libmarpa_dir = current_dir.join(format!("libmarpa-{}", LIBMARPA_VERSION));
-
-    let dst = Config::new(libmarpa_dir).cflag("-fPIC").build().join("lib");
+    env::set_var("CFLAGS", format!("{} -fPIC", env::var("CFLAGS").unwrap_or_default())); // workaround for .cflag("-fPIC") ?
+    let dst = Config::new(libmarpa_dir).build().join("lib");
 
     println!("cargo:rustc-link-search=native={}", dst.display());
     println!("cargo:rustc-link-lib=static=marpa");
